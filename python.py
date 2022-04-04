59808bba7b9d99961b0a9549a93d2586e8d0ba48
kusuma <kusuma.g@thundersoft.com>
2022-04-04 08:30:09 -0700

    basics of python

---
+++ python.py
@@ -0,0 +1,26 @@
+#basics of python
+a=5
+print(a)
+
+print("hello all")
+
+
+'''a=5
+b=6
+c=a*b
+print(c)'''
+
+a=b=c=10
+print(a+b)
+
+#a=input("enter element:'\n'")
+#b=input("enter element:'\n'")
+#print(a*b)
+
+a='thunder'
+b='soft'
+print(a+b)
+
+for i in range(1,11):
+ print(5,'*',i,'=',5*i)
+
