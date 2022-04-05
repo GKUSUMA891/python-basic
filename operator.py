a3dcb16b0c8b1c5d8c9b306df07126f381168a5e
kusuma <kusuma.g@thundersoft.com>
2022-04-05 06:01:20 -0700

    perator precedence

---
+++ operator.py
@@ -0,0 +1,41 @@
+#OPERATRS:
+a=20
+b=30
+c=40
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+if(a==b):
+    print("a is equal to b")
+else:
+    print("a is not equal to b")
+if(a!=b):
+    print("a is not equal to b")
+else:
+     print("a is equal to b")
+d=(a+b)*c/b
+print(d)
+e=a+b
+e+=a
+print(e)
+if(a>=b):
+    print("a is greater then equal to b")
+else:
+    print("a is not greater than equal to b")
+
+a=50
+b=[10,20,30,0]
+if(a in b):
+    print("a is in given list")
+else:
+        print("a is not in given list")
+
+if(a  not in b):
+    print("a is not in given list")
+else:
+        print("a is in given list")
+
+
